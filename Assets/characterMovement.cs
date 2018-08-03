d855164ba1e9e4f3b7c14c69ef3b2fc296ee283a
AIE\adam.bailey <adam.bailey@aie.edu.au>
2018-08-03 16:02:11 +0930

    First Commit of unity project files

---
+++ Assets/characterMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class characterMovement : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        navMeshAgent.destination = target.position;
+	}
+}
